8678a37616bf9236695a0cf22eea10bbe3ffbb6a
dte-23010 <snayak@datopic.com>
2023-06-27 18:07:36 +0530

    Connect firebase to application

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
+import 'package:deeplink_app/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized(); // Initialize Flutter binding
+  // Initialize Firebase
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
